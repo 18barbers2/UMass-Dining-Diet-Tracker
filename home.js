272016574f244849c241ecd6b4a8220acb58a185
18barbers2 <sbarber@umass.edu>
2021-11-06 18:13:39 -0400

    added handler for loading goals

--- home.js
+++ home.js
@@ -1,5 +1,14 @@
-window.addEventListener("load", loadName);
+//window.addEventListener("load", loadName);
+window.addEventListener("load",loadGoals)
 
 function loadName() {
     document.getElementById("welcomeMsg").innerText = `Welcome ${window.sessionStorage.getItem("name")}!`;
+}
+
+function loadGoals() {
+    document.getElementById("calorie-count").innerText = JSON.parse(window.localStorage.getItem("daily_goals"))["calories"];
+    document.getElementById("calorie-count").innerText = JSON.parse(window.localStorage.getItem("daily_goals"))["carbs"];
+    document.getElementById("calorie-count").innerText = JSON.parse(window.localStorage.getItem("daily_goals"))["calories"];
+    document.getElementById("calorie-count").innerText = JSON.parse(window.localStorage.getItem("daily_goals"))["calories"];
+    document.getElementById("calorie-count").innerText = JSON.parse(window.localStorage.getItem("daily_goals"))["calories"];
 }
