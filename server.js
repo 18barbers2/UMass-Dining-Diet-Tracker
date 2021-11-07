52f58a0eafa7c70f77cfd56e5b945520e33a8906
18barbers2 <sbarber@umass.edu>
2021-11-06 23:35:29 -0400

    fixing

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 
-import express from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//import express from "express";
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const port = 8080;
+const express = require("express");
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
