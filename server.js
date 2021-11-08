d1cb013f81eb6c87852155d2de5a490739bd4c13
18barbers2 <sbarber@umass.edu>
2021-11-08 16:46:26 -0500

    changed directory structure

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 'use strict';
 
-//import express from "express";
+import express from "express";
 // import path from 'path';
 // import { fileURLToPath } from 'url';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-const express = require("express");
+//const express = require("express");
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use("/", express.static("public"));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + "/sign-in.html");
