55000aa91c8524276d05587625cbc29793be348e
Sam Barber <47995052+18barbers2@users.noreply.github.com>
2021-11-13 14:50:54 -0500

    values now save from local storage user object, fixed const carbs

--- public/profile.js
+++ public/profile.js
@@ -2,12 +2,12 @@
 
 
 async function updateDailyGoals() {
-    let output = "";
-    let apiLink = "http://localhost:8080/profile/update";
-    
-    console.log("BUTTON CLICKED UPDATING GOALS");
-    const dailyGoals = getDailyGoalValues();
-    window.localStorage.setItem("daily_goals", dailyGoals);
+    // let output = "";
+    //  let apiLink = "http://localhost:8080/profile/update";
+    // console.log("BUTTON CLICKED UPDATING GOALS");
+
+    //TODO:SEND NUTRITIONAL TINGZ TO DATABASE
+    getDailyGoalValues();
 
 }
 
@@ -15,15 +15,25 @@ async function updateDailyGoals() {
 function getDailyGoalValues() {
 
     const calories = document.getElementById("calorie-goal").value;
-    const carbs = document.getElementById("calorie-goal").value;
+    const carbs = document.getElementById("carbs-goal").value;
     const fat = document.getElementById("fat-goal").value;
     const sodium = document.getElementById("sodium-goal").value;
     const cholesterol = document.getElementById("cholesterol-goal").value;
     const sugar = document.getElementById("sugar-goal").value;
     const protein = document.getElementById("protein-goal").value;
+    const weight = document.getElementById("weight").value;
 
-    let nutritionJSON = {"calories": calories, "carbs": carbs, "fat": fat,"sodium": sodium, "cholesterol": cholesterol, "sugar": sugar, "protein": protein};
-    return JSON.stringify(nutritionJSON);
+    let nutritionJSON = {"weight": weight, "calorieLimit": calories, "carbLimit": carbs, "fatLimit": fat,"sodiumLimit": sodium, "cholesterolLimit": cholesterol, "sugarLimit": sugar, "proteinLimit": protein};
+    
+    const user = JSON.parse(storage.getItem("user"));
+
+    for (let item of Object.keys(nutritionJSON)){
+
+        user[item] = nutritionJSON[item];        
+
+    }
+
+    window.localStorage.setItem("user", JSON.stringify(user));
 }
 
 const updateDailyValues = document.getElementById("update-nutrition");
