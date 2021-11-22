b6e97e839347acdfebfc6f44bfa1c0a328832dfe
18barbers2 <sbarber@umass.edu>
2021-11-22 00:43:06 -0500

    added user schema and models folder

---
+++ models/user.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    macroHistory: [{
+        date: String,
+        caloriesTotal: Number,
+        proteinTotal: Number,
+        carbohydratesTotal: Number,
+        cholesterolTotal: Number,
+        fatTotal: Number,
+        sodiumTotal: Number,
+        sugarTotal: Number,
+        weightTotal: Number
+    }],
+    nutritionGoals : {
+        calories: Number,
+        protein: Number,
+        carbohydrates: Number,
+        cholesterol: Number,
+        fat: Number,
+        sodium: Number,
+        sugar: Number
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export {User};
