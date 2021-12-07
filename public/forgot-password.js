2653550ea4d2a27c282f77e78b765b572716b456
18barbers2 <sbarber@umass.edu>
2021-12-06 19:35:21 -0500

    beginning of reset password

--- public/forgot-password.js
+++ public/forgot-password.js
@@ -3,10 +3,15 @@
 const forgotpasswordButton = document.getElementById("resetPassButton");
 forgotpasswordButton.addEventListener("click", resetPassword);
 
+
+function sendResetPassword() {
+
+
+}
 async function resetPassword() {
     let apiLink = `/delete/password`;
 
-    const emailJSON = JSON.stringify({"email": document.getElementById("resetEmail").value});
+    const emailJSON = JSON.stringify({"email": document.getElementById("reset-email").value});
 
     const response = await fetch(apiLink , {
         method: 'POST',
