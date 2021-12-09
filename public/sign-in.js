d9556cee7532c7f8a15749070f9025aeba705f0d
18barbers2 <sbarber@umass.edu>
2021-12-09 00:28:28 -0500

    removed old localstorage functionality

--- public/sign-in.js
+++ public/sign-in.js
@@ -3,26 +3,6 @@
 const loginButton = document.getElementById("login-button");
 loginButton.addEventListener('click', () => loginUser());
 
-// const loginButton = document.getElementById("login-button");
-// loginButton.addEventListener('click', updateName);
-
-// async function updateName() {
-//     let output = "";
-//     let apiLink = "http://localhost:8080/login/" + document.getElementById("username").value + "/";
-//     const response = await fetch(apiLink);
-//     //check if the response was a success
-//     if (response.ok) {
-//         const userJSON = await response.json();
-//         output = userJSON;
-//     } else {
-//         //report error if you cant reach the api
-//         output = "fail";
-//     }
-//     const storage = window.localStorage;
-//     storage.setItem("user", JSON.stringify(output));
-//     window.location.href = '/home';
-// }
-
 async function loginUser() {
 
     const emailAddress = document.getElementById("email").value;
@@ -37,19 +17,6 @@ async function loginUser() {
         headers: {'Content-Type': 'application/json'}
     });
     window.location = file.url;
-
-    // alert(response.ok);
-    // if(response.ok) {
-    //     window.location.href = "/home";
-    //     const userJSON = await response.json();
-    //     const userEmail = userJSON["email"];
-
-    //     const storage = window.localStorage;
-    //     storage.setItem("userEmail", userEmail);
-    // }
-    // else {
-    //     window.location.href = "/sign-in";
-    // }
-    //console.log(response);
+    console.log(file.url);
 }
 
