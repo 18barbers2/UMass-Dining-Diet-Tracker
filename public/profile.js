377f17a57831e38a15389c8bf828d3a8750bbb01
18barbers2 <sbarber@umass.edu>
2021-12-11 18:09:04 -0500

    added average button functionality

--- public/profile.js
+++ public/profile.js
@@ -27,7 +27,7 @@ function getDailyGoalValues() {
 
     // note: could be changed to loop if html is changed
     const calories = document.getElementById("calorie-goal").value;
-    const carbohydrates = document.getElementById("carbs-goal").value;
+    const carbohydrates = document.getElementById("carbohydrates-goal").value;
     const fat = document.getElementById("fat-goal").value;
     const sodium = document.getElementById("sodium-goal").value;
     const cholesterol = document.getElementById("cholesterol-goal").value;
@@ -53,3 +53,25 @@ function getDailyGoalValues() {
     
     const updateDailyValues = document.getElementById("update-nutrition");
     updateDailyValues.addEventListener("click", () => updateDailyGoals());
+
+    document.getElementById("calorie-avg-button").addEventListener("click", ()=> {
+        document.getElementById("calorie-goal").value = 2000;
+    });
+    document.getElementById("carbohydrates-avg-button").addEventListener("click", ()=> {
+        document.getElementById("carbohydrates-goal").value = 300;
+    });
+    document.getElementById("sodium-avg-button").addEventListener("click", ()=> {
+        document.getElementById("sodium-goal").value= 2000;
+    });
+    document.getElementById("cholesterol-avg-button").addEventListener("click", ()=> {
+        document.getElementById("cholesterol-goal").value = 250;
+    });
+    document.getElementById("sugar-avg-button").addEventListener("click", ()=> {
+        document.getElementById("sugar-goal").value = 24;
+    });
+    document.getElementById("protein-avg-button").addEventListener("click", ()=> {
+        document.getElementById("protein-goal").value = 50;
+    });
+    document.getElementById("fat-avg-button").addEventListener("click", ()=> {
+        document.getElementById("fat-goal").value = 50;
+    });
