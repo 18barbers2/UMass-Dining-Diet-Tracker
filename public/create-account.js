f3fca243329ff78ede2d3dddb8bea95b5082a2eb
18barbers2 <sbarber@umass.edu>
2021-12-11 16:48:56 -0500

    added routing functionality for creating an account

--- public/create-account.js
+++ public/create-account.js
@@ -24,7 +24,14 @@ async function sendAccountData() {
         }
     }
 
+<<<<<<< Updated upstream
    
+=======
+    if(response.ok) {
+        window.location = response.url;
+    }
+
+>>>>>>> Stashed changes
 }
 
 function getAccountInfo() {
